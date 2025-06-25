8d82f431ca99df8d279f24400be601f16438c068
Ylber Gashi <ylberg37@gmail.com>
2025-06-25 09:14:10 +0200

    Remove health check route and related configurations from the server and Kubernetes deployment files.

--- apps/server/src/api/client/routes/index.ts
+++ apps/server/src/api/client/routes/index.ts
@@ -4,14 +4,12 @@ import { accountRoutes } from '@colanode/server/api/client/routes/accounts';
 import { avatarRoutes } from '@colanode/server/api/client/routes/avatars';
 import { workspaceRoutes } from '@colanode/server/api/client/routes/workspaces';
 import { socketRoutes } from '@colanode/server/api/client/routes/sockets';
-import { healthRoute } from '@colanode/server/api/client/routes/health';
 
 export const clientRoutes: FastifyPluginCallback = (instance, _, done) => {
   instance.register(socketRoutes, { prefix: '/sockets' });
   instance.register(accountRoutes, { prefix: '/accounts' });
   instance.register(avatarRoutes, { prefix: '/avatars' });
   instance.register(workspaceRoutes, { prefix: '/workspaces' });
-  instance.register(healthRoute, { prefix: '/health' });
 
   done();
 };
