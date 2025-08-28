5f6f545cd6cf2c8f5c69054ac600b06b09e53af8
Hakan Shehu <hakanshehu15@gmail.com>
2025-08-28 19:58:32 +0200

    Load icon and emoji svgs in dom for styling purposes

--- packages/ui/src/components/emojis/emoji-element.tsx
+++ packages/ui/src/components/emojis/emoji-element.tsx
@@ -1,4 +1,5 @@
 import { useApp } from '@colanode/ui/contexts/app';
+import { useLiveQuery } from '@colanode/ui/hooks/use-live-query';
 
 interface EmojiElementProps {
   id: string;
@@ -6,23 +7,50 @@ interface EmojiElementProps {
   onClick?: () => void;
 }
 
+const EmojiElementWeb = ({ id, className, onClick }: EmojiElementProps) => {
+  return (
+    <svg className={className} onClick={onClick}>
+      <use href={`/assets/emojis.svg#${id}`} />
+    </svg>
+  );
+};
+
+const EmojiElementDesktop = ({ id, className, onClick }: EmojiElementProps) => {
+  const svgQuery = useLiveQuery({
+    type: 'emoji.svg.get',
+    id,
+  });
+
+  if (svgQuery.isLoading) {
+    return null;
+  }
+
+  if (svgQuery.isError) {
+    return null;
+  }
+
+  const svg = svgQuery.data;
+  if (!svg) {
+    return null;
+  }
+
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+      dangerouslySetInnerHTML={{ __html: svg }}
+    />
+  );
+};
+
 export const EmojiElement = ({ id, className, onClick }: EmojiElementProps) => {
   const app = useApp();
 
   if (app.type === 'web') {
-    return (
-      <svg className={className} onClick={onClick}>
-        <use href={`/assets/emojis.svg#${id}`} />
-      </svg>
-    );
+    return <EmojiElementWeb id={id} className={className} onClick={onClick} />;
   }
 
   return (
-    <img
-      src={`local://emojis/${id}`}
-      className={className}
-      onClick={onClick}
-      alt={id}
-    />
+    <EmojiElementDesktop id={id} className={className} onClick={onClick} />
   );
 };
--- packages/ui/src/components/icons/icon-element.tsx
+++ packages/ui/src/components/icons/icon-element.tsx
@@ -1,20 +1,49 @@
 import { useApp } from '@colanode/ui/contexts/app';
+import { useLiveQuery } from '@colanode/ui/hooks/use-live-query';
 
 interface IconElementProps {
   id: string;
   className?: string;
 }
 
+const IconElementWeb = ({ id, className }: IconElementProps) => {
+  return (
+    <svg className={className}>
+      <use href={`/assets/icons.svg#${id}`} />
+    </svg>
+  );
+};
+
+const IconElementDesktop = ({ id, className }: IconElementProps) => {
+  const svgQuery = useLiveQuery({
+    type: 'icon.svg.get',
+    id,
+  });
+
+  if (svgQuery.isLoading) {
+    return null;
+  }
+
+  if (svgQuery.isError) {
+    return null;
+  }
+
+  const svg = svgQuery.data;
+  if (!svg) {
+    return null;
+  }
+
+  return (
+    <div className={className} dangerouslySetInnerHTML={{ __html: svg }} />
+  );
+};
+
 export const IconElement = ({ id, className }: IconElementProps) => {
   const app = useApp();
 
   if (app.type === 'web') {
-    return (
-      <svg className={className}>
-        <use href={`/assets/icons.svg#${id}`} />
-      </svg>
-    );
+    return <IconElementWeb id={id} className={className} />;
   }
 
-  return <img src={`local://icons/${id}`} className={className} />;
+  return <IconElementDesktop id={id} className={className} />;
 };
