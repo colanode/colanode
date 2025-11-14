db2422e3739e7e5568f5f98877b48fb9159ebc54
Hakan Shehu <hakanshehu15@gmail.com>
2025-11-13 23:21:36 -0800

    Add animation for login screen and improve app loading screen

--- packages/ui/src/components/app/app-loading.tsx
+++ packages/ui/src/components/app/app-loading.tsx
@@ -5,11 +5,11 @@ export const AppLoading = () => {
   return (
     <div className="min-w-screen flex h-full min-h-screen w-full items-center justify-center">
       <DelayedComponent>
-        <div className="flex flex-col items-center gap-8 text-center">
-          <h2 className="font-satoshi text-4xl">loading your workspaces</h2>
-          <div>
-            <Spinner />
-          </div>
+        <div className="flex flex-row items-center gap-6">
+          <h2 className="font-satoshi tracking-tight text-4xl">
+            loading your workspace
+          </h2>
+          <Spinner className="size-6" />
         </div>
       </DelayedComponent>
     </div>
--- packages/ui/src/components/auth/auth-layout.tsx
+++ packages/ui/src/components/auth/auth-layout.tsx
@@ -16,8 +16,10 @@ export const AuthLayout = () => {
       <div className="w-full flex lg:flex-row flex-col items-center justify-center lg:gap-40 gap-20">
         <div className="flex flex-col items-center justify-center bg-background px-6 py-12">
           <div className="flex flex-row items-center">
-            <ColanodeLogo className="size-16 lg:size-50" />
-            <p className="font-antonio text-3xl">
+            <div className="logo-draw-animation">
+              <ColanodeLogo className="size-16 lg:size-50" />
+            </div>
+            <p className="font-satoshi text-3xl tracking-tight">
               Your all-in-one <br /> collaboration platform
             </p>
           </div>
