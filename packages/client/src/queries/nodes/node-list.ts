0920835b785d07a479a97d123dcb274cd1712ffe
Hakan Shehu <git@hakanshehu.com>
2026-01-07 11:14:43 +0100

    MInor updates

--- packages/client/src/queries/nodes/node-list.ts
+++ packages/client/src/queries/nodes/node-list.ts
@@ -1,8 +1,13 @@
+import { ParsedOrderBy, SimpleComparison } from '@tanstack/db';
+
 import { LocalNode } from '@colanode/client/types/nodes';
 
 export type NodeListQueryInput = {
   type: 'node.list';
   userId: string;
+  filters: Array<SimpleComparison>;
+  sorts: Array<ParsedOrderBy>;
+  limit?: number;
 };
 
 declare module '@colanode/client/queries' {
