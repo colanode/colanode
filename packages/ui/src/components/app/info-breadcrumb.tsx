d8a3df9e060a3196c3b441aa8366dba8dc00b726
Hakan Shehu <git@hakanshehu.com>
2026-02-09 13:20:34 +0100

    Add app info route and page (#320)

---
+++ packages/ui/src/components/app/info-breadcrumb.tsx
@@ -0,0 +1,8 @@
+import { BreadcrumbItem } from '@colanode/ui/components/layouts/containers/breadcrumb-item';
+import { defaultIcons } from '@colanode/ui/lib/assets';
+
+export const InfoBreadcrumb = () => {
+  return (
+    <BreadcrumbItem id="info" avatar={defaultIcons.apps} name="Info" />
+  );
+};
---
+++ packages/ui/src/components/app/info-tab.tsx
@@ -0,0 +1,6 @@
+import { TabItem } from '@colanode/ui/components/layouts/tabs/tab-item';
+import { defaultIcons } from '@colanode/ui/lib/assets';
+
+export const InfoTab = () => {
+  return <TabItem id="info" avatar={defaultIcons.apps} name="Info" />;
+};
